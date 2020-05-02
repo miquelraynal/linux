d02f9f7b1305b24938cef2013f9cab1beffbad3a
Miquel Raynal <miquel.raynal@bootlin.com>
2020-05-02 21:26:47 +0200

    lib/bch: Export the logic deriving the locator polynomial
    It may be useful sometimes to just verify the validity of the
    derivation of the locator polynomial without actually looking for an
    accurate error detection.
    
    With BCH algorithm, deriving the locator polynomial syndrome is pretty
    fast while looking for the exact count and location of each error is
    pretty time consuming.
    
    Let's extract the part of bch_decode() which does this derivation so
    that we can use it without spending too much time finding the
    roots. The naming is not just "find the number of bitflips" because
    this new function does not fully cover the error cases and finding the
    real number of bitflips implies calling find_poly_roots() which is
    long lasting.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- lib/bch.c
+++ lib/bch.c
@@ -951,6 +951,52 @@ static int chien_search(struct bch_control *bch, unsigned int len,
 #define find_poly_roots(_p, _k, _elp, _loc) chien_search(_p, len, _elp, _loc)
 #endif /* USE_CHIEN_SEARCH */
 
+int bch_compute_error_locator_polynomial(struct bch_control *bch,
+					 const uint8_t *data, unsigned int len,
+					 const uint8_t *recv_ecc,
+					 const uint8_t *calc_ecc,
+					 const unsigned int *syn)
+{
+	const unsigned int ecc_words = BCH_ECC_WORDS(bch);
+	uint32_t sum;
+	int err, i;
+
+	/* sanity check: make sure data length can be handled */
+	if (8*len > (bch->n-bch->ecc_bits))
+		return -EINVAL;
+
+	/* if caller does not provide syndromes, compute them */
+	if (!syn) {
+		if (!calc_ecc) {
+			/* compute received data ecc into an internal buffer */
+			if (!data || !recv_ecc)
+				return -EINVAL;
+			bch_encode(bch, data, len, NULL);
+		} else {
+			/* load provided calculated ecc */
+			load_ecc8(bch, bch->ecc_buf, calc_ecc);
+		}
+		/* load received ecc or assume it was XORed in calc_ecc */
+		if (recv_ecc) {
+			load_ecc8(bch, bch->ecc_buf2, recv_ecc);
+			/* XOR received and calculated ecc */
+			for (i = 0, sum = 0; i < (int)ecc_words; i++) {
+				bch->ecc_buf[i] ^= bch->ecc_buf2[i];
+				sum |= bch->ecc_buf[i];
+			}
+			if (!sum)
+				/* no error found */
+				return 0;
+		}
+		compute_syndromes(bch, bch->ecc_buf, bch->syn);
+		syn = bch->syn;
+	}
+
+	err = compute_error_locator_polynomial(bch, syn);
+
+	return (err >= 0) ? err : -EBADMSG;
+}
+
 /**
  * bch_decode - decode received codeword and find bit error locations
  * @bch:      BCH control structure
@@ -997,43 +1043,11 @@ int bch_decode(struct bch_control *bch, const uint8_t *data, unsigned int len,
 	       const uint8_t *recv_ecc, const uint8_t *calc_ecc,
 	       const unsigned int *syn, unsigned int *errloc)
 {
-	const unsigned int ecc_words = BCH_ECC_WORDS(bch);
 	unsigned int nbits;
-	int i, err, nroots;
-	uint32_t sum;
+	int nroots, err, i;
 
-	/* sanity check: make sure data length can be handled */
-	if (8*len > (bch->n-bch->ecc_bits))
-		return -EINVAL;
-
-	/* if caller does not provide syndromes, compute them */
-	if (!syn) {
-		if (!calc_ecc) {
-			/* compute received data ecc into an internal buffer */
-			if (!data || !recv_ecc)
-				return -EINVAL;
-			bch_encode(bch, data, len, NULL);
-		} else {
-			/* load provided calculated ecc */
-			load_ecc8(bch, bch->ecc_buf, calc_ecc);
-		}
-		/* load received ecc or assume it was XORed in calc_ecc */
-		if (recv_ecc) {
-			load_ecc8(bch, bch->ecc_buf2, recv_ecc);
-			/* XOR received and calculated ecc */
-			for (i = 0, sum = 0; i < (int)ecc_words; i++) {
-				bch->ecc_buf[i] ^= bch->ecc_buf2[i];
-				sum |= bch->ecc_buf[i];
-			}
-			if (!sum)
-				/* no error found */
-				return 0;
-		}
-		compute_syndromes(bch, bch->ecc_buf, bch->syn);
-		syn = bch->syn;
-	}
-
-	err = compute_error_locator_polynomial(bch, syn);
+	err = bch_compute_error_locator_polynomial(bch, data, len, recv_ecc,
+						   calc_ecc, syn);
 	if (err > 0) {
 		nroots = find_poly_roots(bch, 1, bch->elp, errloc);
 		if (err != nroots)
