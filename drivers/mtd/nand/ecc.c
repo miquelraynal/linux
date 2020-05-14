59211b5edb3168b30a642f7c2069a905ef99c9ec
Miquel Raynal <miquel.raynal@bootlin.com>
2020-05-14 09:34:53 +0200

    mtd: nand: ecc: Add a page I/O abstraction
    Provide a way for external ECC engines to provide hooks to actually
    do the I/Os if they need to do it themselves.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/nand/ecc.c
+++ drivers/mtd/nand/ecc.c
@@ -145,6 +145,21 @@ int nand_ecc_prepare_io_req(struct nand_device *nand,
 EXPORT_SYMBOL(nand_ecc_prepare_io_req);
 
 /**
+ * nand_ecc_do_page_io - Do the page I/O
+ * @nand: the NAND device
+ * @req: the I/O request
+ */
+int nand_ecc_do_page_io(struct nand_device *nand,
+			const struct nand_page_io_req *req)
+{
+	if (nand->ecc.engine && nand->ecc.engine->ops->do_page_io)
+		return nand->ecc.engine->ops->do_page_io(nand, req);
+
+	return nanddev_do_raw_page_io(nand, req);
+}
+EXPORT_SYMBOL(nand_ecc_do_page_io);
+
+/**
  * nand_ecc_finish_io_req - Finish an I/O request
  * @nand: the NAND device
  * @req: the I/O request
