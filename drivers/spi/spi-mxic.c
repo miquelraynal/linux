e7805f35a7b84635e5ac33d386413cf1022b2ab4
Miquel Raynal <miquel.raynal@bootlin.com>
2021-12-07 14:03:51 +0100

    spi: mxic: Use spi_mem_generic_supports_op()
    This driver can be simplified a little bit by using
    spi_mem_generic_supports_op() instead of the
    spi_mem_default/dtr_supports_op() couple. The all_false boolean is
    inverted to become a dtr boolean, which tacks if at least one of the
    operation member uses dtr mode. The idea behind this change is to
    simplify the introduction of the pipelined ECC engine.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/spi/spi-mxic.c
+++ drivers/spi/spi-mxic.c
@@ -452,7 +452,7 @@ static ssize_t mxic_spi_mem_dirmap_write(struct spi_mem_dirmap_desc *desc,
 static bool mxic_spi_mem_supports_op(struct spi_mem *mem,
 				     const struct spi_mem_op *op)
 {
-	bool all_false;
+	bool dtr;
 
 	if (op->data.buswidth > 8 || op->addr.buswidth > 8 ||
 	    op->dummy.buswidth > 8 || op->cmd.buswidth > 8)
@@ -465,13 +465,9 @@ static bool mxic_spi_mem_supports_op(struct spi_mem *mem,
 	if (op->addr.nbytes > 7)
 		return false;
 
-	all_false = !op->cmd.dtr && !op->addr.dtr && !op->dummy.dtr &&
-		    !op->data.dtr;
+	dtr = op->cmd.dtr || op->addr.dtr || op->dummy.dtr || op->data.dtr;
 
-	if (all_false)
-		return spi_mem_default_supports_op(mem, op);
-	else
-		return spi_mem_dtr_supports_op(mem, op);
+	return spi_mem_generic_supports_op(mem, op, dtr, false);
 }
 
 static int mxic_spi_mem_dirmap_create(struct spi_mem_dirmap_desc *desc)
